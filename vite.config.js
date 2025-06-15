05ff09594bd12f625bc803388fa45153f908e312
bharath <bharathsai257@gmail.com>
2025-06-15 17:25:48 +0530

    11

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,6 @@ export default defineConfig({
     react(),
     //  tailwindcss()
   ],
-  base: process.env.VITE_BASE_PATH || "/shopping-cart",
+  base: "/",
+  // process.env.VITE_BASE_PATH || "/shopping-cart",
 });
